2b78fd49a3a002a55c6ca19221974e8567c680d0
khaliuss <khalidkhushmadov@gmail.com>
2026-02-16 20:13:01 +0300

    L3.7 finished

---
+++ OOP/src/big_project/GUI.kt
@@ -0,0 +1,6 @@
+package big_project
+
+fun main() {
+    val  rec = Rectangle()
+    rec.draw()
+}
---
+++ OOP/src/big_project/Rectangle.kt
@@ -0,0 +1,26 @@
+package big_project
+
+class Rectangle {
+
+    val width: Int
+    val height: Int
+
+    constructor() : this(0)
+
+    constructor (width: Int, height: Int) {
+        this.width = width
+        this.height = height
+    }
+
+    constructor(size: Int) : this(size, size)
+
+    fun draw() {
+        repeat(height) {
+            repeat(width) {
+                print("* ")
+            }
+            print("\n")
+        }
+    }
+
+}
