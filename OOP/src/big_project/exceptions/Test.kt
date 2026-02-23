f1401f73ad0be80cd4ead7e0b1e1a89a6069fbbc
khaliuss <khalidkhushmadov@gmail.com>
2026-02-23 19:22:30 +0300

    L4.14  Try Catch  finished

---
+++ OOP/src/big_project/exceptions/Test.kt
@@ -0,0 +1,26 @@
+package big_project.exceptions
+
+fun main() {
+
+//    try {
+//        val a :Int = readln().toInt()
+//        val b:Int = readln().toInt()
+//        println(a / b)
+//
+//    }catch (exception:ArithmeticException){
+//        println("You can't divide by zero")
+//    }catch (exception: NumberFormatException){
+//        println("Wrong input")
+//    }catch (exception: Throwable){
+//        println("Common error")
+//    }
+
+//    val list = arrayListOf<Int>(1,2,3)
+//    list[5]
+
+
+    println("After try-catch")
+
+
+
+}
