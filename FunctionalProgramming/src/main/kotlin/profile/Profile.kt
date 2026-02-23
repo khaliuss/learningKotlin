d1552973700864be646bf2ef23f942b7032509e6
khaliuss <khalidkhushmadov@gmail.com>
2026-02-23 22:16:17 +0300

    L5.3-4 p1

--- FunctionalProgramming/src/main/kotlin/profile/Profile.kt
+++ FunctionalProgramming/src/main/kotlin/profile/Profile.kt
@@ -8,9 +8,43 @@ fun main() {
 
     val persons = ProfilesRepository.profiles
 
-    for (person in persons){
+    var filtered = filterOlderThen25(persons)
+    filtered = filterNameStartsWithA(filtered)
+    filtered = filterMale(filtered)
+
+    for (person in filtered){
         println(person)
     }
 
+}
+
+fun filterMale(persons: List<Person>): List<Person> {
+    val newPersons = mutableListOf<Person>()
+    for (person in persons) {
+        if (person.gender == Gender.MALE){
+            newPersons.add(person)
+        }
+    }
+    return newPersons
+}
+
+fun filterNameStartsWithA(persons: List<Person>): List<Person> {
+    val newPersons = mutableListOf<Person>()
+    for (person in persons) {
+        if (person.firstName.startsWith("A")){
+            newPersons.add(person)
+        }
+    }
+    return newPersons
+}
+
+fun filterOlderThen25(persons: List<Person>): List<Person> {
+    val newPersons = mutableListOf<Person>()
+    for (person in persons) {
+        if (person.age<25){
+            newPersons.add(person)
+        }
+    }
+    return newPersons
 }
 
