974ac932b58f6e6efc4003ae669644587d1c20f5
khaliuss <khalidkhushmadov@gmail.com>
2026-02-24 15:09:41 +0300

    L5.10 p1

--- FunctionalProgramming/src/main/kotlin/profile/Profile.kt
+++ FunctionalProgramming/src/main/kotlin/profile/Profile.kt
@@ -6,6 +6,8 @@ import org.example.extentions.myForEach
 
 fun main() {
 
+    print("Please put id: ")
+    val id = readln().trim().toInt()
 
     ProfilesRepository.profiles
         .filter { it.age > 25 }
@@ -15,9 +17,24 @@ fun main() {
         .map { it.copy(age = it.age + 1) }
         .sortedBy { it.firstName }
         .sortedByDescending { it.age }
-        .myForEach { println(it) }
+        .showEmail(id)
+//        .myForEach { println(it) }
+
 //        .forEach { println(it) }
 
+}
+
+inline fun List<Person>.showEmail(id:Int): Unit{
+    var temp = ""
+    for (item in this){
+        if (item.id == id){
+            temp = item.email
+            break
+        }else{
+            temp = "The user is not found"
+        }
+    }
+    println(temp)
 }
 
 
