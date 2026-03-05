07abb7d6654dc3cdf616030851742c275b72a9e1
khaliuss <khalidkhushmadov@gmail.com>
2026-03-06 02:44:29 +0300

    L8.25 (channels & Debounce)

--- FlowAndChannels/src/main/kotlin/dictionary/Display.kt
+++ FlowAndChannels/src/main/kotlin/dictionary/Display.kt
@@ -2,11 +2,18 @@ package org.example.dictionary
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
@@ -17,9 +24,10 @@ import java.awt.event.KeyAdapter
 import java.awt.event.KeyEvent
 import javax.swing.*
 
+@OptIn(FlowPreview::class)
 object Display {
 
-    private lateinit var queries: Flow<String>
+    private val queries = Channel<String>()
     val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
     val repository = Repository
 
@@ -57,7 +65,9 @@ object Display {
     }
 
     private fun loadingDefinitions() {
-
+        scope.launch {
+            queries.send(searchField.text.trim())
+        }
     }
 
     fun show() {
@@ -66,33 +76,36 @@ object Display {
 
     init {
 
-        //method 1
-        /*scope.launch {
-            queries
-                .onEach {
-                    searchButton.isEnabled = false
-                    resultArea.text = "Searching the word...."
-                }.map {
-                    repository.loadingDefinition(it)
-                }.map {
-                    it.joinToString("\n\n").ifEmpty { "Not Found" }
-                }.onEach {
-                    resultArea.text = it
-                    searchButton.isEnabled = true
-                }.collect()
-        }*/
+        //Method 1 Channel to Flow
+        /*flow {
+            queries.consumeEach {
+                emit(it)
+            }
+        }.onEach {
+            searchButton.isEnabled = false
+            resultArea.text = "Searching the word...."
+        }.map {
+            repository.loadingDefinition(it)
+        }.map {
+            it.joinToString("\n\n").ifEmpty { "Not Found" }
+        }.onEach {
+            resultArea.text = it
+            searchButton.isEnabled = true
+        }.launchIn(scope)*/
 
 
-        //method 2
-        queries
+        //Method 2 Channel to Flow
+        queries.consumeAsFlow()
             .onEach {
                 searchButton.isEnabled = false
                 resultArea.text = "Searching the word...."
-            }.map {
+            }.debounce(500)
+            .map {
                 repository.loadingDefinition(it)
             }.map {
                 it.joinToString("\n\n").ifEmpty { "Not Found" }
             }.onEach {
+
                 resultArea.text = it
                 searchButton.isEnabled = true
             }.launchIn(scope)
