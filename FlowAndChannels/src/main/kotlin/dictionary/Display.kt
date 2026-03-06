d35cba99b38de4c2a68d3ac75ef916e6acb8ab27
khaliuss <khalidkhushmadov@gmail.com>
2026-03-06 11:41:17 +0300

    L8.30 (Screen state as Flow )

--- FlowAndChannels/src/main/kotlin/dictionary/Display.kt
+++ FlowAndChannels/src/main/kotlin/dictionary/Display.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.consumeAsFlow
 import kotlinx.coroutines.flow.debounce
@@ -28,6 +29,8 @@ import javax.swing.*
 object Display {
 
     private val queries = Channel<String>()
+    private val state = MutableSharedFlow<ScreenState>()
+
     val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
     val repository = Repository
 
@@ -76,39 +79,46 @@ object Display {
 
     init {
 
-        //Method 1 Channel to Flow
-        /*flow {
-            queries.consumeEach {
-                emit(it)
-            }
-        }.onEach {
-            searchButton.isEnabled = false
-            resultArea.text = "Searching the word...."
-        }.map {
-            repository.loadingDefinition(it)
-        }.map {
-            it.joinToString("\n\n").ifEmpty { "Not Found" }
-        }.onEach {
-            resultArea.text = it
-            searchButton.isEnabled = true
-        }.launchIn(scope)*/
-
-
-        //Method 2 Channel to Flow
         queries.consumeAsFlow()
             .onEach {
-                searchButton.isEnabled = false
-                resultArea.text = "Searching the word...."
+                state.emit(ScreenState.Initial)
             }.debounce(500)
             .map {
-                repository.loadingDefinition(it)
-            }.map {
-                it.joinToString("\n\n").ifEmpty { "Not Found" }
-            }.onEach {
-
-                resultArea.text = it
-                searchButton.isEnabled = true
+                if (it.isEmpty()){
+                    state.emit(ScreenState.Initial)
+                }else{
+                    val result = repository.loadingDefinition(it)
+                    if (result.isEmpty()){
+                        state.emit(ScreenState.NotFound)
+                    }else{
+                        state.emit(ScreenState.DefinitionsLoaded(result))
+                    }
+                }
             }.launchIn(scope)
+
+
+        state.onStart {
+            emit(ScreenState.Initial)
+        }.onEach {
+            when(it){
+                is ScreenState.DefinitionsLoaded -> {
+                    searchButton.isEnabled = true
+                    resultArea.text = it.definition.joinToString("\n\n")
+                }
+                ScreenState.Initial -> {
+                    searchButton.isEnabled = false
+                    resultArea.text = ""
+                }
+                ScreenState.Loading -> {
+                    searchButton.isEnabled = false
+                    resultArea.text = "Searching the word...."
+                }
+                ScreenState.NotFound -> {
+                    searchButton.isEnabled = true
+                    resultArea.text = "Not Found"
+                }
+            }
+        }.launchIn(scope)
 
     }
 
---
+++ FlowAndChannels/src/main/kotlin/dictionary/ScreenState.kt
@@ -0,0 +1,11 @@
+package org.example.dictionary
+
+sealed interface ScreenState {
+
+    data object  Initial: ScreenState
+    data object  Loading: ScreenState
+    data object  NotFound: ScreenState
+    data class  DefinitionsLoaded(val definition: List<String>): ScreenState
+
+
+}
